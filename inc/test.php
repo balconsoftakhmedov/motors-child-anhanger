0b7af14dac47335cfeeb1df4bf417a1f86d9e4bb
BK <akhmedov@balcomsoft.com>
2023-09-24 16:59:26 +0500

    dd

--- inc/test.php
+++ inc/test.php
@@ -1,7 +1,7 @@
 <?php
 
 // The provided CSV data as a string.
-$csvData = <<<EOD
+$csvData   = <<<EOD
 "trailer","from","until","calculated","correct","test","weekend ?","explanation of wrong calculation","solution";
 "8","10.03 . , 18:00","12.03 . , 21:00","151","57","","Fr - So","2 x weekend + 1 day","weekend";
 "6a","11.03 . , 10:00","11.03 . , 17:00","42","31","","Sa","weekend","1 day";
@@ -25,7 +25,7 @@ weekend + 4 hours";
 "11","04.08 . , 09:00","07.08 . , 09:00","200","101","","Fr - Mo","???","4 hours ( fr ) + weekend";
 "1","04.05 . , 08:00","05.05 . , 10:00","60","55","","Do - Fr","↓ Jump to latest message","";
 EOD;
-$products = [
+$products  = [
 	"1"   => [ "4 Std." => "25,00 €", "24 Std." => "30,00 €", "Wochenende" => "45,00 €" ],
 	"2"   => [ "4 Std." => "16,00 €", "24 Std." => "20,00 €", "Wochenende" => "30,00 €" ],
 	"3"   => [ "4 Std." => "22,00 €", "24 Std." => "28,00 €", "Wochenende" => "39,00 €" ],
@@ -36,17 +36,17 @@ $products = [
 	"7"   => [ "4 Std." => "29,00 €", "24 Std." => "39,00 €", "Wochenende" => "59,00 €" ],
 	"8"   => [ "4 Std." => "27,00 €", "24 Std." => "37,00 €", "Wochenende" => "57,00 €" ],
 	"9"   => [ "4 Std." => "32,00 €", "24 Std." => "42,00 €", "Wochenende" => "62,00 €" ],
-	"10"  => [  "4 Std." => "0","24 Std." => "43,00 €", "Wochenende" => "80,00 €" ],
+	"10"  => [ "4 Std." => "0", "24 Std." => "43,00 €", "Wochenende" => "80,00 €" ],
 	"11"  => [ "4 Std." => "27,00 €", "24 Std." => "52,00 €", "Wochenende" => "74,00 €" ],
 	"11A" => [ "4 Std." => "30,00 €", "24 Std." => "58,00 €", "Wochenende" => "78,00 €" ],
-	"13"  => [ "4 Std." => "0","24 Std." => "85,00 €", "Wochenende" => "150,00 €" ],
+	"13"  => [ "4 Std." => "0", "24 Std." => "85,00 €", "Wochenende" => "150,00 €" ],
 	"14"  => [ "4 Std." => "20,00 €", "24 Std." => "38,00 €", "Wochenende" => "59,00 €" ],
 	"17"  => [ "4 Std." => "35,00 €", "24 Std." => "70,00 €", "Wochenende" => "130,00 €" ],
 	"18"  => [ "4 Std." => "24,00 €", "24 Std." => "38,00 €", "Wochenende" => "59,00 €" ]
 ];
-$csvData = preg_replace( '/(\d{2}\.\d{2}) \. , (\d{2}:\d{2})/', '$1.2023 $2', $csvData );
-$lines = explode( ";", $csvData );
-$headers = str_getcsv( array_shift( $lines ) );
+$csvData   = preg_replace( '/(\d{2}\.\d{2}) \. , (\d{2}:\d{2})/', '$1.2023 $2', $csvData );
+$lines     = explode( ";", $csvData );
+$headers   = str_getcsv( array_shift( $lines ) );
 $dataArray = [];
 foreach ( $lines as $line ) {
 	if ( ! empty( trim( $line ) ) ) { // To make sure we're not processing empty lines
@@ -55,18 +55,17 @@ foreach ( $lines as $line ) {
 }
 foreach ( $dataArray as &$item ) {
 	if ( isset( $products[ strtoupper( $item["trailer"] ) ] ) ) {
-		$id           = strtoupper( $item["trailer"] );
-
-		if ( $id > 0  && '14.07.2023 13:00' == $item["from"] ) {
-					$prices       = $products[ strtoupper( $item["trailer"] ) ];
-		$hour4Price   = intval( $prices['4 Std.'] );
-		$dayPrice     = intval( $prices['24 Std.'] );
-		$weekendPrice = intval( $prices['Wochenende'] );
-		$correct      = $item["correct"];
-			$totalCost = calculateRentalCost( $item["from"], $item["until"], $hour4Price, $dayPrice, $weekendPrice );
-			$solution  = $item['solution'];
-			$from = $item["from"];
-			$until = $item["until"];
+		$id = strtoupper( $item["trailer"] );
+		if ( $id > 0 && '14.07.2023 13:00' == $item["from"] ) {
+			$prices       = $products[ strtoupper( $item["trailer"] ) ];
+			$hour4Price   = intval( $prices['4 Std.'] );
+			$dayPrice     = intval( $prices['24 Std.'] );
+			$weekendPrice = intval( $prices['Wochenende'] );
+			$correct      = $item["correct"];
+			$totalCost    = calculateRentalCost( $item["from"], $item["until"], $hour4Price, $dayPrice, $weekendPrice );
+			$solution     = $item['solution'];
+			$from         = $item["from"];
+			$until        = $item["until"];
 			if ( $correct == $totalCost ) {
 				//echo " Correct price - $totalCost ";
 				//	echo "<br/> id=$id, hour4Price=$hour4Price, dayPrice=$dayPrice, weekendPrice=$weekendPrice, Total cost is $totalCost<br/><br/>";
@@ -91,12 +90,10 @@ function diff_time( $pickupDateTime, $returnDateTime ) {
 function calculateRentalCost( $pickupDate, $returnDate, $hour4Price, $dayPrice, $weekendPrice ) {
 	$totalCost      = 0;
 	$pickupDateTime = new DateTime( $pickupDate );
-	$startDateTime = new DateTime( $pickupDate ); ;
-
+	$startDateTime  = new DateTime( $pickupDate );;
 	$returnDateTime = new DateTime( $returnDate );
 	$duration       = $pickupDateTime->diff( $returnDateTime );
-
-	$totalHours     = diff_time( $pickupDateTime, $returnDateTime );
+	$totalHours = diff_time( $pickupDateTime, $returnDateTime );
 	if ( $totalHours <= 4 ) {
 		return $hour4Price;
 	}
@@ -104,59 +101,52 @@ function calculateRentalCost( $pickupDate, $returnDate, $hour4Price, $dayPrice, 
 
 		$dayOfWeek = $pickupDateTime->format( 'w' );
 		$hourOfDay = (int) $pickupDateTime->format( 'H' );
-
 		$currenttotalHours = diff_time( $pickupDateTime, $returnDateTime );
-	//stm_show ( "dayOfWeek = $dayOfWeek, pickupDateTime == " . $pickupDateTime->format( 'Y-m-d H:i:s' ) . " totalHours= $currenttotalHours" . " <br/>  ");
+		//stm_show ( "dayOfWeek = $dayOfWeek, pickupDateTime == " . $pickupDateTime->format( 'Y-m-d H:i:s' ) . " totalHours= $currenttotalHours" . " <br/>  ");
 		if ( ( ( $dayOfWeek == 5 && $hourOfDay >= 12 ) ) && ( $currenttotalHours > 24 ) ) {
 			$nextInterval = new DateTime( $pickupDateTime->format( 'Y-m-d H:i:s' ) );
-
-
 			if ( $nextInterval <= $returnDateTime ) {
 
-				if ($startDateTime < $pickupDateTime) {
+				if ( $startDateTime < $pickupDateTime ) {
 					$fdayOfWeek = $startDateTime->format( 'w' );
 					$fhourOfDay = (int) $startDateTime->format( 'H' );
-					$totalCost += $hour4Price;
-						stm_show (" friady 4hrs pay $hour4Price ");
+					$totalCost  += $hour4Price;
+					stm_show( " friady 4hrs pay $hour4Price " );
 				}
-
 				$pickupDateTime->modify( '+48 hours' );
 				$totalCost += $weekendPrice;
-
-				stm_show (" friady hourOfDay $hourOfDay $weekendPrice");
+				stm_show( " friady hourOfDay $hourOfDay $weekendPrice" );
 			} else {
 				$pickupDateTime->modify( '+48 hours' );
 			}
-		//	stm_show ("weekendPrice =$weekendPrice <br/> ");
-		} else if ( ( ( $dayOfWeek == 5 && (   $hourOfDay < 12  ) )) && ( $currenttotalHours > 24 ) ) {
+			//	stm_show ("weekendPrice =$weekendPrice <br/> ");
+		} else if ( ( ( $dayOfWeek == 5 && ( $hourOfDay < 12 ) ) ) && ( $currenttotalHours > 24 ) ) {
 			$nextInterval = new DateTime( $pickupDateTime->format( 'Y-m-d H:i:s' ) );
-				if ( $nextInterval <= $returnDateTime ) {
+			if ( $nextInterval <= $returnDateTime ) {
 
-				if ($startDateTime <= $pickupDateTime) {
+				if ( $startDateTime <= $pickupDateTime ) {
 					$fdayOfWeek = $startDateTime->format( 'w' );
 					$fhourOfDay = (int) $startDateTime->format( 'H' );
-					$totalCost += $hour4Price;
+					$totalCost  += $hour4Price;
 				}
-
 				$pickupDateTime->modify( '+48 hours' );
 				$totalCost += $weekendPrice;
-
-				stm_show (" rrrr hourOfDay $hourOfDay $weekendPrice");
+				stm_show( " rrrr hourOfDay $hourOfDay $weekendPrice" );
 			} else {
 				$pickupDateTime->modify( '+48 hours' );
 			}
+		} else if ( ( ( $dayOfWeek == 1 && ( $hourOfDay < 12 ) ) ) && ( $currenttotalHours > 24 ) ) {
+			$totalCost += $hour4Price;
+			$pickupDateTime->modify( '+24 hours' );
 		} else if ( $dayOfWeek != 0 ) {
 
-	//	stm_show ( "<br/> r " . __LINE__ . "  dayOfWeek=" . $dayOfWeek . " <br/>  dur h =" . $duration->h . " <br/>  ");
 			if ( $currenttotalHours > 4 ) {
 				$totalCost += $dayPrice;
-				stm_show ( "day 1 " . $dayPrice . " <br/>  ");
+				stm_show( "day 1 " . $dayPrice . " <br/>  " );
 			} elseif ( $currenttotalHours <= 4 && $currenttotalHours > 0 ) {
 				$totalCost += $hour4Price;
-				stm_show ("hour4Price == $hour4Price <br/> ");
+				stm_show( "hour4Price == $hour4Price <br/> " );
 			}
-
-
 			$pickupDateTime->modify( '+24 hours' );
 
 		} else {
@@ -168,9 +158,10 @@ function calculateRentalCost( $pickupDate, $returnDate, $hour4Price, $dayPrice, 
 	return $totalCost;
 }
 
-function stm_show ($val){
-	echo $val." <br />";
+function stm_show( $val ) {
+	//echo $val." <br />";
 }
+
 ?>
 
 
